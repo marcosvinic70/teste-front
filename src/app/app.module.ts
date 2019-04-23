408b32d153822a80d2ccacf692db23500bcfda4a
Marcos Oliveira <marcos.oliveira@gt4w.com.br>
2019-04-22 21:52:44 -0300

    base projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		SideNavComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/side-nav/side-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-side-nav',
+	templateUrl: './side-nav.component.pug',
+	styleUrls: ['./side-nav.component.scss']
+})
+export class SideNavComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+}
